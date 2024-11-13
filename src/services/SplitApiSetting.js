f3efe9798f7696c98af6e531126ff931e4882e9d
usama6688 <usamayasin46@gmail.com>
2024-11-14 04:17:09 +0500

    ui

--- src/services/SplitApiSetting.js
+++ src/services/SplitApiSetting.js
@@ -21,7 +21,7 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
-  if (result.error && result.error.status == 403) {
+  if (result.error && (result.error.status == 401 || result.error.status == 403)) {
     api.dispatch(userLogout(null));
     window.location.replace(PATHS.signin);
   }
